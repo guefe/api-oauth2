32d68c0fdd110452a616c44bfc5965a79cae9503
Carlos Estévez <guefe5@gmail.com>
2018-01-21 02:26:05 +0100

    changed CORS configuration

--- src/main/java/com/cen/server/configuration/ResourceServerConfig.java
+++ src/main/java/com/cen/server/configuration/ResourceServerConfig.java
@@ -1,19 +1,13 @@
 package com.cen.server.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.session.SessionManagementFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 
 @Configuration
@@ -30,36 +24,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .and()
                 .headers().frameOptions().disable()
                 .and()
-                .addFilterBefore(corsFilter(), SessionManagementFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers(entryPoints).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .csrf().disable();
+                .csrf().disable().addFilterAfter(new SimpleCorsFilter(), BasicAuthenticationFilter.class);
     }
 
-    @Bean
-    public CorsFilter corsFilter(){
-        return new CorsFilter();
-    }
-
-    class CorsFilter extends OncePerRequestFilter {
-
-        @Override
-        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
-            response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
-            if ("OPTIONS".equals(request.getMethod())) {
-                response.setStatus(HttpServletResponse.SC_OK);
-            } else {
-                filterChain.doFilter(request, response);
-            }
-        }
-    }
 
 }
