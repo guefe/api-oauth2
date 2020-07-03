c8840f4a3402809167f0d71a3b0778ebf4f45b8d
guefe5 <guefe5@gmail.com>
2020-07-03 21:36:37 +0200

    added lombok

--- src/main/java/com/cen/server/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cen/server/service/impl/ProductServiceImpl.java
@@ -3,9 +3,8 @@ package com.cen.server.service.impl;
 import com.cen.server.entity.Product;
 import com.cen.server.repository.ProductRepository;
 import com.cen.server.service.ProductService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
@@ -14,31 +13,31 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-    Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class.getName());
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     @Override
     public List<Product> findProducts(Product filter) {
-        logger.debug("Retrieving products");
+        log.debug("Retrieving products");
         if (filter != null && !filter.isEmpty()) {
             return productRepository.findAll(Example.of(filter));
-        }else {
+        } else {
             return productRepository.findAll();
         }
     }
 
     @Override
     public Optional<Product> findProduct(Integer id) {
-        logger.debug("Retrieving product with id: "+id);
+        log.debug("Retrieving product with id: " + id);
         return productRepository.findById(id);
     }
 
     @Override
     public void updateProduct(Product product) {
-        logger.debug("updating");
+        log.debug("updating");
         this.productRepository.save(product);
     }
 
@@ -46,14 +45,14 @@ public class ProductServiceImpl implements ProductService {
     public void deleteProduct(Integer id) {
         this.productRepository.deleteById(id);
         this.productRepository.getOne(id);
-        logger.debug("Deleted product with id: " + id);
+        log.debug("Deleted product with id: {}", id);
     }
 
     @Override
     public Product createProduct(Product product) {
         product.setCreationDate(new Date());
         Product newProduct = this.productRepository.save(product);
-        logger.debug("Inserted new product with id: " + newProduct.getId());
+        log.debug("Inserted new product with id: " + newProduct.getId());
         return newProduct;
     }
 }
